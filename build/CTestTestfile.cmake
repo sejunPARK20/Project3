a7366346046af8bfda6f086fd16b456e70e32dd4
sejunPARK20 <sejuuun20@gmail.com>
2024-09-23 16:04:26 +0900

    Added ydlidar_ros_driver submodule

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/pj/rs_ws/src
+# Build directory: /home/pj/rs_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_slam_package")
+subdirs("ydlidar_ros_driver")
---
+++ build/my_slam_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pj/rs_ws/src/my_slam_package
+# Build directory: /home/pj/rs_ws/build/my_slam_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
